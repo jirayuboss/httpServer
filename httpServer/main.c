2e9aa509df10c6da4e60c0fc3d6762dbddf221e2
jirayuboss <59011599@kmitl.ac.th>
2018-02-16 09:46:50 +0700

    Initial Commit

---
+++ httpServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  httpServer
+//
+//  Created by Jirayu Promsongwong on 16/2/2561 BE.
+//  Copyright © 2561 KMITL. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
